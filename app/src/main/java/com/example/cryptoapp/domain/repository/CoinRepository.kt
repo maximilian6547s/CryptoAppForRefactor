35040713267ffed7c1397a6bc415cc598ca74684
Maxim Sakharov <sakharov.ma@sberleasing.ru>
2022-11-22 11:54:11 -0600

    add repository interface

---
+++ app/src/main/java/com/example/cryptoapp/domain/repository/CoinRepository.kt
@@ -0,0 +1,11 @@
+package com.example.cryptoapp.domain.repository
+
+import androidx.lifecycle.LiveData
+import com.example.cryptoapp.domain.model.CoinInfo
+
+interface CoinRepository {
+
+    fun getCoinInfoList(): LiveData<List<CoinInfo>>
+
+    fun getCoinInfo(fromSymbol: String): LiveData<CoinInfo>
+}
