ed4d9c63ea45b41d75c683442931fd3c70239b91
Maxim Sakharov <sakharov.ma@sberleasing.ru>
2022-11-22 12:48:01 -0600

    add repository implementation

---
+++ app/src/main/java/com/example/cryptoapp/domain/usecases/LoadDataUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.cryptoapp.domain.usecases
+
+import com.example.cryptoapp.domain.repository.CoinRepository
+
+class LoadDataUseCase(
+    private val repository: CoinRepository
+) {
+
+    suspend operator fun invoke()  = repository.loadData()
+}
