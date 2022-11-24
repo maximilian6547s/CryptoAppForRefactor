1979cf2ecf1c8a07792300c545ab752cf2764abe
Maxim Sakharov <sakharov.ma@sberleasing.ru>
2022-11-24 12:39:23 -0600

    way to inject multiple workers

--- app/src/main/java/com/example/cryptoapp/presentation/CoinApp.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinApp.kt
@@ -2,14 +2,14 @@ package com.example.cryptoapp.presentation
 
 import android.app.Application
 import androidx.work.Configuration
-import com.example.cryptoapp.data.workers.RefreshDataWorkerFactory
+import com.example.cryptoapp.data.workers.CoinWorkerFactory
 import com.example.cryptoapp.di.DaggerApplicationComponent
 import javax.inject.Inject
 
 class CoinApp : Application(), Configuration.Provider {
 
     @Inject
-    lateinit var workerFactory: RefreshDataWorkerFactory
+    lateinit var coinWorkerFactory: CoinWorkerFactory
 
     val component by lazy {
         DaggerApplicationComponent.factory().create(this)
@@ -22,7 +22,7 @@ class CoinApp : Application(), Configuration.Provider {
 
     override fun getWorkManagerConfiguration(): Configuration {
         return Configuration.Builder()
-            .setWorkerFactory(workerFactory)
+            .setWorkerFactory(coinWorkerFactory)
             .build()
     }
 }
